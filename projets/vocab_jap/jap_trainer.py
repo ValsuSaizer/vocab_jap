46b240cc3bb61e398b5459b927b69d7bec48d7fd
DESCORMES <pierre.descormes@sts.group>
2022-10-10 16:09:02 +0200

    ajout anciens fichiers

---
+++ projets/vocab_jap/jap_trainer.py
@@ -0,0 +1,8 @@
+idee de programme
+
+faire un programme qui te demande quelle fiche de vocabulaire de japonais tu veux faire
+une fois le choix fait, il te dit quel mot tu dois ecrire
+le programme choisit aleatoirement les mots et ne doit pas selectionner 2 fois le meme mot
+le programme va chercher les mots dans des fichiers a part (txt par exemple)
+Possibilite d'avoir la reponse du mot en cours en ecrivant "answer" ou * par exemple
+Appuyer sur la touche entree fait afficher le mot suivant
