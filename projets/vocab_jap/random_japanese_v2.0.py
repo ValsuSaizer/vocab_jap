2d0762dbe6b2856db6b16f7e77f5174bb30b0147
ValsuSaizer <caillouxx007@gmail.com>
2022-10-11 08:29:51 +0200

    Ajout du menu de base pour le choix d'une fiche de vocabulaire.

--- projets/vocab_jap/random_japanese_v2.0.py
+++ projets/vocab_jap/random_japanese_v2.0.py
@@ -2,6 +2,7 @@ from random import randint
 from csv import reader,writer
 from os import listdir
 
+#Premier prompt avec le premier choix et boucle try pour les exceptions.
 print( "Bienvenu sur ce progamme d'entrainement au vocabulaire japonais !" )
 while True:
     print( "Comment souhaites-tu travailler ?\n1 - Travailler une seule fiche ?\n2 - Travailler toutes les fiches à la fois ?\n3 - Travailler une selection de fiche ?" )
@@ -15,14 +16,29 @@ while True:
         continue
     break
 
+#Menu dans le cas d'une seule fiche de vocabulaire.
 if choice == 1:
     folder = listdir( "fiches" )
+    print( "Voici les fiches disponible." )
     for x in folder:
-        print( x )
-    choice = 
-elif choice == 2:
+        print( "{} - {}".format( folder.index(x) + 1, x ) )
+    while True:
+        try:
+            choice = int( input( "Choix: " ) )
+        except ValueError:
+            print( "Hey, ce n'est pas un choix valide." )
+            continue
+        if choice not in range(1, len( folder ) + 1 ):
+            print( "Ayonyonyon... essaie une des valeurs proposées s'il te plait    ." )
+            continue
+        break
+#Ouverture de la fiche de vocabulaire avec la boucle.
 
-elif choice == 3:
+
+
+#elif choice == 2:
+
+#elif choice == 3:
 
 
 
